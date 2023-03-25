dad5c6d2ace15ae70f23905641e6dabdac3aef63
Kateryna18 <mikolaichukkate@icloud.com>
2023-03-25 22:18:21 +0200

    added async/await and axios

--- src/ImagesApiService.js
+++ src/ImagesApiService.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export default class ImagesApiService {
     constructor() {
         this.searchQuery = '';
@@ -5,23 +7,17 @@ export default class ImagesApiService {
 
     }
 
+    async fetchImages() {
+        try {
+            
+            const url = `https://pixabay.com/api/?key=34700186-64fa17513eb3359bd5c913c6b&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`
+            const response = await axios.get(url);
+            this.incrementPage();
+            return response.data.hits;
 
-    fetchImages() {
-    // Create for parametrs!!!
-    const url = `https://pixabay.com/api/?key=34700186-64fa17513eb3359bd5c913c6b&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`
-
-    return fetch(url)
-    .then(response => {
-        if(!response.ok) {
-            throw new Error(response.status);
-        }
-        return response.json();
-    })
-    .then(images => {
-        this.incrementPage();
-
-        return images.hits;
-    });
+        } catch (error) {
+            console.error(error);
+        }   
 }
 
 incrementPage() {
--- src/index.js
+++ src/index.js
@@ -21,36 +21,35 @@ loadMoreBtn.refs.button.addEventListener('click', onShowMoreImages);
 
 function onShowImages(event) {
   event.preventDefault();
-  loadMoreBtn.hide()
+  clearBoxGallery();
+  loadMoreBtn.hide();
   
-
   imagesApiService.query = event.currentTarget.elements.searchQuery.value;
-
-  
   imagesApiService.resetPage();
-  imagesApiService.fetchImages().then(images => {
   
-    console.log(images);
+  imagesApiService.fetchImages().then(images => {
     if (images.length === 0) {
       return Notiflix.Notify.info(
         'Sorry, there are no images matching your search query. Please try again.'
       );
     } 
 
-    
     renderMarkupImagesCard(images);
     loadMoreBtn.show()
+    
   }); 
+
+  
 }
 
 function onShowMoreImages(event) {
   event.preventDefault();
+  
   imagesApiService.fetchImages().then(renderMarkupImagesCard);
 
 }
 
 function renderMarkupImagesCard(images) {
-  clearBoxGallery();
   const markupImagesCard = images.map(
     ({
       webformatURL,
@@ -63,7 +62,7 @@ function renderMarkupImagesCard(images) {
     }) => {
       return `
         <div class="photo-card">
-         <img src="${webformatURL}" alt="${tags}" loading="lazy" width=50px />
+         <img src="${webformatURL}" alt="${tags}" loading="lazy" width=250px />
          <div class="info">
            <p class="info-item"><b>Likes: ${likes}</b></p>
            <p class="info-item"><b>Views: ${views}</b></p>
